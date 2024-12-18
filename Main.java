9f195509794c9360d021c6e8fecd7be52ce61327
jquie <jacquelynphan03@gmail.com>
2024-12-18 18:10:48 +0000

    done

--- Main.java
+++ Main.java
@@ -4,6 +4,31 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    // Problem 1
+    /*
+    System.out.println("Enter two numbers");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    while (a <= b)
+    {
+      if (a % 2 == 1)
+      {
+        System.out.println(a + " ");
+      }
+      a++;
+    }
+    */
+   System.out.println("Enter a positive number");
+   int num = sc.nextInt();
+   int degree = 0;
+   while (num > 0)
+   {
+    int last_digit = num % 10;
+    num /= 10;
+    double place_value = Math.pow(10, degree);
+    System.out.println((int) (last_digit * place_value));
+    degree++;
+   }
   }
 }
